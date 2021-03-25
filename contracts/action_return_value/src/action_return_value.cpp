fe93a7a6c605c485a405435d1a132f3b8050520a
김준철 <47551453+hysn19@users.noreply.github.com>
2021-03-25 16:01:32 +0900

    Update action_return_value.cpp

--- contracts/action_return_value/src/action_return_value.cpp
+++ contracts/action_return_value/src/action_return_value.cpp
@@ -2,6 +2,12 @@
 
 using namespace eosio;
 
+struct action_response
+{
+  uint16_t id;
+  std::pair<int, std::string> status;
+};
+
 class [[eosio::contract]] action_return_value : public contract {
   public:
       using contract::contract;
@@ -9,6 +15,25 @@ class [[eosio::contract]] action_return_value : public contract {
       [[eosio::action]]
       int sum(int valueA, int valueB) {
          return valueA + valueB;
+      }
+
+      [[eosio::action]]
+      std::string hello() {
+          return "hello, EOSIO!";
+      }
+
+      [[eosio::action]]
+      action_response checkwithrv(name nm) {
+         print_f("name : %\n", nm);
+         action_response results;
+         results.id = 1;
+         if (nm == "hello"_n) {
+            results.status = {0, "Validation has passed."};
+         }
+         else {
+            results.status = {1, "Input param `name` not equal to `hello`."};
+         }
+         return results; // the `set_action_return_value` intrinsic is invoked automatically here
       }
 
   private:
